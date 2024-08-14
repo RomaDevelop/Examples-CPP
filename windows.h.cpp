0114a40f4d3e41791dd6a1f77723dac296725086
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-08-14 17:47:04 +0300

    Update

--- windows.h.cpp
+++ windows.h.cpp
@@ -1,13 +1,13 @@
-	//#include <windows.h>
-	MEMORYSTATUSEX statex;
-	statex.dwLength = sizeof (statex);
-	GlobalMemoryStatusEx(&statex);
+//#include <windows.h>
+MEMORYSTATUSEX statex;
+statex.dwLength = sizeof (statex);
+GlobalMemoryStatusEx(&statex);
 
-	if(sumSizez > statex.ullAvailPhys / 40000000)
-	{
-		if(QMessageBox::question(this, "Большой объем данных", "В указали файлов суммарны объемом " + QSn(sumSizez / (1024*1024))
-								 + " Mb.\nОперативной памяти свободно " + QSn(statex.ullAvailPhys / (1024*1024))
-								 + " Mb.\nВозможно переполнение оперативной памяти. Продолжать?",
-								 QMessageBox::Yes|QMessageBox::No) == QMessageBox::No)
-			return;
-	}
+if(sumSizez > statex.ullAvailPhys / 40000000)
+{
+	if(QMessageBox::question(this, "Большой объем данных", "В указали файлов суммарны объемом " + QSn(sumSizez / (1024*1024))
+							 + " Mb.\nОперативной памяти свободно " + QSn(statex.ullAvailPhys / (1024*1024))
+							 + " Mb.\nВозможно переполнение оперативной памяти. Продолжать?",
+							 QMessageBox::Yes|QMessageBox::No) == QMessageBox::No)
+		return;
+}
