3c5a3e5e3deca392e99ab7a225d1f290d88f8423
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-09-26 14:42:48 +0300

    Update

--- crono.cpp
+++ crono.cpp
@@ -10,11 +10,13 @@ long long get_nanosec_duration(auto &start_time)
     return elapsed.count();
 }
 
-void print_result(auto &start_time, string prefix)
+void print_result(auto &start_time, string prefix = "")
 {
     string elapsed_str = to_string(get_nanosec_duration(start_time));
     for(int i=elapsed_str.size()-1, n=1; i>=0; i--, n++) if(n%3 == 0 && i!=0) elapsed_str.insert(i," ");
-    cout << prefix+" " << elapsed_str << endl;
+	if(!prefix.empty())
+		cout << prefix+" ";
+	cout << elapsed_str << endl;
 }
 
 int main() {
