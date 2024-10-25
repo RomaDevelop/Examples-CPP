9e1e2590d4e4e119e3268efe7b3fb718ec4c5561
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-10-25 15:17:55 +0300

    Update

--- iterator.cpp
+++ iterator.cpp
@@ -4,7 +4,26 @@ for(auto wIter = words.begin(); wIter!=words.end();)
 	if(WidgetAddSettings::all.contains(*wIter))
 	{
 		AddAddSetting(*wIter);
-		words.erase(wIter);
+		words.erase(wIter);     
 	}
 	else ++wIter;
+}
+
+// !!! нельзя использовать base() обратных итераторов для удаления !!!
+
+// обратный цикл с прямыми итераторами, можно удалять
+auto it = prev(v.end());
+while(true)
+{
+    bool first (it == v.begin());
+    cout << *it << " ";
+    if(first) break;
+    --it;
+}
+
+for(auto it = prev(v.end()); true; --it)
+{
+    bool first (it == v.begin());
+    cout << *it << " ";
+    if(first) break;
 }
